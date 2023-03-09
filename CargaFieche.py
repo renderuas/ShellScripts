977ad257110cc15586759d974a0e195c879b0c43
renderuas <renderuas@gmail.com>
2023-03-09 14:49:24 +0100

    anadido fichero si no existe

--- CargaFieche.py
+++ CargaFieche.py
@@ -7,6 +7,17 @@ if os.path.exists(file_path):
   print('El fichero existe')
 else:
   print('El fichero no existe')
+  
+  datos = {
+  'name': 'John',
+  'age': 50,
+  'is_married': False,
+  'profession': None,
+  'hobbies': ['travelling', 'photography']
+}
+
+with open('user.json', 'w') as archivo:
+    json.dump(datos, archivo)
 
 with open('user.json') as user_file:
   file_contents = user_file.read()
